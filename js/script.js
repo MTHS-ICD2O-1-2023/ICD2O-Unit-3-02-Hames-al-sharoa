9801aaf06b4e9cbcfe86709c78d63c1feec5b85a
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-04-09 18:36:34 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 hames.al-sharoa All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: hames.al-sharoa
+// Created on: apr 2024
 // This file contains the JS functions for index.html
+
+function calculateVolume() {
+
+  const length = parseFloat(document.getElementById('length').value);
+
+  const width = parseFloat(document.getElementById('width').value);
+
+  const height = parseFloat(document.getElementById('height').value);
+
+
+
+  // Calculate the volume using the formula: V = (L x W x H) / 3
+
+  const volume = (length * width * height) / 3;
+
+
+
+  // Display the result
+
+  document.getElementById('result').textContent = `Volume is: ${volume.toFixed(2)} mm³`;
+
+}
